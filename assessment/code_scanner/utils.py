78b79f088ad9357eef16e66194ef43be96194306
Sri Tikkireddy <sri.tikkireddy@databricks.com>
2023-08-14 06:50:53 -0400

    remove bad comma

--- assessment/code_scanner/utils.py
+++ assessment/code_scanner/utils.py
@@ -6,7 +6,7 @@ from assessment.code_scanner.scan import in_dbx_notebook, get_dbutils
 def get_ws_client(default_profile="uc-assessment-azure"):
     if in_dbx_notebook():
         _dbutils = get_dbutils()
-        host = _dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().getOrElse(None),
+        host = _dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().getOrElse(None)
         token = _dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().getOrElse(None)
         return WorkspaceClient(host=host, token=token)
     return WorkspaceClient(profile=default_profile)
