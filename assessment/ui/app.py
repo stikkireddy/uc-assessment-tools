4e32b3d54ab88e4021c5f18b733acb8b56e75055
Gary Diana <gary.diana@databricks.com>
2023-08-16 08:22:56 -0400

    Code cleanup and formatting.

--- assessment/ui/app.py
+++ assessment/ui/app.py
@@ -1,7 +1,6 @@
 import io
 import os
 import tempfile
-import time
 import traceback
 import zipfile
 from datetime import datetime
@@ -136,7 +135,7 @@ def RepoScanner():
             solara.Info(f"Loading... Current File: {curr_file} "
                         f"Progress: {progress}/{max_progress} files scanned")
             if max_progress > 0 and progress > 0:
-                solara.ProgressLinear((progress/max_progress) * 100)
+                solara.ProgressLinear((progress / max_progress) * 100)
             else:
                 solara.ProgressLinear(True)
         elif issues is not None:
